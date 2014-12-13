ad922165863e11a30ae7b5a34dd4565421dd3a6c
Exxarion <Apluscomics@live.com>
2014-12-12 20:32:03 -0500

    Create Utilities.php

---
+++ src/WorldGM/Utilities.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WorldGM;
+
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class Utilities {
+
+    public static function getWorldGamemode(Config $config, $world) {
+        return (isset($config->get(WorldGM::CONFIG_WORLDS)[$world])) ? $config->get(WorldGM::CONFIG_WORLDS)[$world] 
+        $this->getDefaultGamemode();
+    }
+
+    public static function setWorldGamemode(Config $config, $world, $gamemode) {
+        $worlds = $config->get(WorldGM::CONFIG_WORLDS);
+        $worlds[$world] = $gamemode;
+        $config->set(WorldGM::CONFIG_WORLDS, $worlds);
+        $config->save();
+    }
+
+    public static function unsetWorldGamemode(Config $config, $world) {
+        $worlds = $config->get(WorldGM::CONFIG_WORLDS);
+        unset($worlds[$world]);
+        $config->set(WorldGM::CONFIG_WORLDS, $worlds);
+        $config->save();
+    }
+
+    public static function removeprop(Config $config, $arrname, $value) {
+        if (in_array(strtolower($value), array_map('strtolower', $conf = $config->get($arrname)))) {
+            $config->set($arrname, array_diff($conf, array($value)));
+            $config->save();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function addprop(Config $config, $arrname, $value) {
+        if (!in_array(strtolower($value), array_map('strtolower', $conf = $config->get($arrname)))) {
+            $arr = $config->get($arrname);
+            $arr[] = $value;
+            $config->set($arrname, $arr);
+            $config->save();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
