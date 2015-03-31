3843897a07fab239e2b943f9bb7b98c615d6efeb
Blumieare Exxarion <Apluscomics@live.com>
2015-03-31 15:52:09 -0400

    Clean up and improvements

--- src/WorldGM/PlayerEventListener.php
+++ src/WorldGM/PlayerEventListener.php
@@ -8,6 +8,23 @@ use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 
+/*
+ __        __         _     _  ____ __  __  
+ \ \      / /__  _ __| | __| |/ ___|  \/  | 
+  \ \ /\ / / _ \| '__| |/ _` | |  _| |\/| | 
+   \ V  V / (_) | |  | | (_| | |_| | |  | | 
+    \_/\_/ \___/|_|  |_|\__,_|\____|_|  |_| 
+               | |__  _   _                 
+               | '_ \| | | |                
+               | |_) | |_| |                
+  _____        |_.__/ \__, | _              
+ | ____|_  ____  ____ |___/_(_) ___  _ __   
+ |  _| \ \/ /\ \/ / _` | '__| |/ _ \| '_ \  
+ | |___ >  <  >  < (_| | |  | | (_) | | | | 
+ |_____/_/\_\/_/\_\__,_|_|  |_|\___/|_| |_| 
+                                            
+*/
+
 class PlayerEventListener implements Listener {
 
     private $plugin;
@@ -16,9 +33,6 @@ class PlayerEventListener implements Listener {
         $this->plugin = $plugin;
     }
 
-    /**
-     * @param EntityLevelChangeEvent $event
-     */
     public static function onLevelChange(EntityLevelChangeEvent $event) {
         $entity = $event->getEntity();
         if ($entity instanceof Player) {
@@ -26,17 +40,10 @@ class PlayerEventListener implements Listener {
         }
     }
 
-    /**
-     * @param PlayerRespawnEvent $event
-     */
     public static function onRespawn(PlayerRespawnEvent $event) {
         $this->plugin->checkPlayer($event->getPlayer());
     }
 
-    /**
-     * @param PlayerQuitEvent $event
-     * @ignoreCancelled true
-     */
     public static function onQuit(PlayerQuitEvent $event) {
         $this->plugin->checkPlayer($event->getPlayer());
     }
